cbbc49d677a4ec79401b72bb760f02cc8567f804
Jennifer Akaba <sharazade.akaba@hotmail.fr>
2019-02-11 08:29:44 +0100

    ajout seesion strart() pour faire fonctionner la déconnexion

--- logout.php
+++ logout.php
@@ -1,4 +1,13 @@
  <?php
+// Initialisation de la session.
+// Si vous utilisez un autre nom
+// session_name("autrenom")
+session_start();
+
+// Détruit toutes les variables de session
+$_SESSION = array();
+
+
 // Finalement, on détruit la session.
 session_destroy();
 header ('location: acceuil.php');
