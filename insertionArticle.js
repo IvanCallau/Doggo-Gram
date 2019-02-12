24e372afac6d46a23c07a75578b1224bb592f3df
rahmatullahahmadi23 <rahmatullahahmadi500@gmail.com>
2019-02-12 10:40:10 +0100

    J'ai modifié la page insertionArticle.php. J'ai ajouté insertionArticle.css et insertionArticle.js

---
+++ insertionArticle.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#file").on("change", function(e){
+        var files = $(this)[0].files;
+
+        if(files.length >= 2){
+            $("#label_span").text(files.length + "files ready to upload");
+        }
+        else{
+
+        var filename =e.target.value.split('\\').pop();
+        $("#label_span").text(filename);
+    }
+    });
+        
+    
+});
