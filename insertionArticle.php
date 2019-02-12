6ef4df1df0d8dfc4057bbf59e8d9c157d7da1b8a
rahmatullahahmadi23 <rahmatullahahmadi500@gmail.com>
2019-02-12 12:54:49 +0100

    les pages insertionArticle.php et insertionArticle.css sont pretes

--- insertionArticle.php
+++ insertionArticle.php
@@ -9,6 +9,7 @@ if (isset($_SESSION['user_id'] )){
     require"header.php";
 }
 
+
 require "connexion.php";
 
 $appli = new Connexion();
@@ -18,34 +19,48 @@ $id = $_GET["id"];
 
 
 ?>
-        <title>Doggo-gram - Insertion Article</title>
-        <link rel="stylesheet" href="insertionArticle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doggo-gram - Insertion Article</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="insertionArticle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+
+
 
     <?php
 
     echo '<form method="POST" action="enregistrementArticle.php?id=' . $id . '" enctype="multipart/form-data">';
 
     ?>
-            <div class="champs col-12 col-sm-12">
-						<h3 class="">
-                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
-							Photo:<input id="photo" type="file" name="photo" required>
-						</h3>
-					</div>
-                <div class="form-group">
-                    <label for="contenu"> 
-                                <!-- id = contenu-->
-                        Contenu:<input id="contenu" type="text" name="contenu" required>
-                    </label>  
-                </div>
-                <button type="submit" class="btn btn-primary">
-                    Valider
-                </button>
-            </form>
+<!--         la prrtie input photo  -->
+        <div class="form-div">
+            
+                <label type="hidden" for="file" name="MAX_FILE_SIZE" value="1048576" class="input-label">
+                <i class="fas fa-upload"></i><span id="label_span">
+                Select a file to upload </span></label>
+                <input id="file" type="file" name="photo" required>
+           
         </div>
-
+        <!-- la partie textarea  -->       
+        <div id="textarea">
+            <label for="contenu" id="center"><span id="contenu1"><strong>Contenu: </strong></span> 
+                <textarea id="contenu" type="text" name="contenu" wrap="hard" required></textarea>
+            </label>   
+        </div>
+        <!-- Le button de Valider -->
+        <div id="button">
+            <button type="submit" class="btn btn-primary center">Valider</button>
+        </div>
+                    
+    </form>     
+ 
 <?php
 
 require "footer.php";
 
 ?>
+
+
+<script src="insertionArticle.js"></script>
+
