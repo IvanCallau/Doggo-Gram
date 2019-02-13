2e162d6e240f89096db4790dba22a523233dc9bc
Ivan <ivan.callau@outlook.com>
2019-02-13 15:50:09 +0100

    Implementation de fonction qui empeche les utilisateur non enregistrede voir ou inserer des commentaires.

--- article.php
+++ article.php
@@ -27,17 +27,23 @@ if (isset($_GET["id"])) {
 $id = $_GET["id"];
 
 $id_user = null;
+
+$insertion = 0;
     
 if (isset($_SESSION['user_id'] )){
     //barre de navigation pour utilisateur logged
     include ("loggedHeader.php");
 
     $id_user = $_SESSION['user_id'];
+
+    $insertion = 1;
+
 }
 else {
-    include "header.php";
-}
 
+    include "header.php";
+
+}
 
 //récupération des commentaires dans BDD
 $commentaire = $appli->getAllCommentaire($_GET["id"]);
@@ -63,43 +69,42 @@ $commentaire = $appli->getAllCommentaire($_GET["id"]);
                             '.$article->getTexteArticle().'
                         </p>'. "</div>"
                     .'<div class="row justify-content-md-center"><p class="col-lg-5 text-right">' .$article->getDateParutionArticle().'</p></div>'; 
-                
-            ?>  
 
-           <form id="submit" method="post" action=<?php echo "submitCommentaire.php?id=".$id ?>>
-            <section class="well">
-                <div class="form-group">
-                    <label for="comment"  class="text-left">
-                        Comment:
-                    </label>
+                if ($insertion == 1) {
 
-                    <textarea name="texteCommentaire" type="text" class="form-control" wrap="hard"  rows="3" id="comment" required></textarea>
+                    echo '<form id="submit" method="post" action="submitCommentaire.php?id=' . $id . '">
+                            <section class="well">
+                                <div class="form-group">
+                                    <label for="comment"  class="text-left">
+                                        Comment:
+                                    </label>
+
+                            <textarea name="texteCommentaire" type="text" class="form-control" wrap="hard"  rows="3" id="comment" required></textarea>
 
                    
 
-                    <div class="text-right">
-                        <button type="submit" class="btn btn-primary">
-                            Valider
-                        </button>
-                    </div>
-                </div>
-                </form>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">
+                                    Valider
+                                </button>
+                            </div>
+                        </div>
+                    </form>
 
-            <div style = "max-height:500px; overflow:scroll;">
-
-                <?php
+                    <div style = "max-height:500px; overflow:scroll;">';
         
-                foreach ($commentaire as $value){
+                    foreach ($commentaire as $value){
 
-                    echo '<div class="input-group">
-                            <p>' . $value->getAuteur() . ':&nbsp&nbsp&nbsp' . $value->getTexteCommentaire() . '</br>
-                            </p>
-                          </div>
+                        echo '<div class="input-group">
+                                <p>' . $value->getAuteur() . ':&nbsp&nbsp&nbsp' . $value->getTexteCommentaire() . '</br>
+                                </p>
+                              </div>
 
-                          <div class="text-right">
-                            <p>' . $value->getDateParutionCommentaire() . '</br>
-                            </p>
-                          </div>';
+                              <div class="text-right">
+                                <p>' . $value->getDateParutionCommentaire() . '</br>
+                                </p>
+                              </div>';
+                    }
                 }
 
                 ?>
